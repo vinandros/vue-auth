49d81d2be0c43357dbbcc08e776205036df9cd48
vinandros <vinandros@gmail.com>
2021-11-22 17:38:18 -0600

    Register View

--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {}
-})
+  actions: {
+    register({ commit }, credentials) {
+      return axios
+        .post("//localhost:3000/register", credentials)
+        .then(({ data }) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+});
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form @submit.prevent="register">
+            <label for="name">Name:</label>
+            <input v-model="name" type="text" name="name" id="name" value>
+
+            <label for="email">Email:</label>
+            <input v-model="email" type="email" name="email" id="email" value>
+
+            <label for="password">Password:</label>
+            <input v-model="password" type="password" name="password" id="password" value>
+
+            <button type="submit" name="button">Register</button>
+        </form>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        register(){
+            this.$store.dispatch('register',{
+                name:this.name,
+                email: this.email,
+                password: this.password
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
